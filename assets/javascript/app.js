297b415ea8ac89c4f0bf0b82f981b839555b0414
Eric <ericsteinman@me.com>
2018-01-12 16:41:08 -0600

    Code is finished. Just need to add comments to make it clearer for people reading the code.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,17 +7,15 @@ $( document ).ready(function(){
   $("button").click(function(){
     $("#questions").show();
     $("#start").hide();
+    run();
+
 
   // Score Variables
   var numCorrect = 0;
   var numIncorrect = 0;
   var numUnanswered = 0;
 
-  // Send the scores to the "end" div to display on the page
-  $(".numCorrect").html("Correct Answers: " + numCorrect);
-  $(".numIncorrect").html("Incorrect Answers: " + numIncorrect);
-  $(".numUnanswered").html("Unanswered: " + numUnanswered);
-
+  // Empty User Answer Variables
   var userAnswer1 = null;
   var userAnswer2 = null;
   var userAnswer3 = null;
@@ -25,35 +23,57 @@ $( document ).ready(function(){
   var userAnswer5 = null;
   var userAnswer6 = null;
 
-  var correctAnswer1 = "correct";
-  var correctAnswer2 = "correct";
-  var correctAnswer3 = "correct";
-  var correctAnswer4 = "correct";
-  var correctAnswer5 = "correct";
-  var correctAnswer6 = "correct";
+    // Question 1: Registering user input...
+    $("input:radio[name=q1]").click(function(){
+      $(userAnswer1).empty();
+      userAnswer1 = $('input:radio[name=q1]:checked').val();
+      console.log(userAnswer1);
 
-  $("input:radio[name=q1]").click(function(){
-     userAnswer1 = $('input:radio[name=q1]:checked').val();
-     console.log(userAnswer1);
-       if (userAnswer1 == "correct") {
-         numCorrect++;
-         console.log(numCorrect);
-         $(".numCorrect").html("Correct Answers: " + numCorrect);
+    });
 
-        }
+    // Question 2: Registering user input...
+    $("input:radio[name=q2]").click(function(){
+      $(userAnswer2).empty();
+      userAnswer2 = $('input:radio[name=q2]:checked').val();
+      console.log(userAnswer2);
 
-       else if (userAnswer1 == "wrong") {
-         numIncorrect++;
-         console.log(numIncorrect);
-         $(".numIncorrect").html("Incorrect Answers: " + numIncorrect);
+    });
+    // Question 3: Registering user input...
+    $("input:radio[name=q3]").click(function(){
+      $(userAnswer3).empty();
+      userAnswer3 = $('input:radio[name=q3]:checked').val();
+      console.log(userAnswer3);
 
-        }
+    });
 
-});
+    // Question 4: Registering user input...
+    $("input:radio[name=q4]").click(function(){
+      $(userAnswer4).empty();
+      userAnswer4 = $('input:radio[name=q4]:checked').val();
+      console.log(userAnswer4);
+
+    });
+
+    // Question 5: Registering user input...
+    $("input:radio[name=q5]").click(function(){
+      $(userAnswer5).empty();
+      userAnswer5 = $('input:radio[name=q5]:checked').val();
+      console.log(userAnswer5);
+
+    });
+
+    // Question 6: Registering user input...
+    $("input:radio[name=q6]").click(function(){
+      $(userAnswer6).empty();
+      userAnswer6 = $('input:radio[name=q6]:checked').val();
+      console.log(userAnswer6);
+
+
+    });
 
 
 // Countdown variable
-var number = 5;
+var number = 30;
 
 //  Variable that will hold our interval ID when we execute
 //  the "run" function
@@ -63,29 +83,164 @@ var intervalId;
 //  that runs the decrement function once a second.
 function run() {
   intervalId = setInterval(decrement, 1000);
+
     }
 
     //  The decrement function.
     function decrement() {
+      
+      //  Show the number in the #show-number tag.
+      $("#count-down").html("<h2> Time Remaining: " + number + " Seconds </h2>");
 
       //  Decrease number by one.
       number--;
 
-      //  Show the number in the #show-number tag.
-      $("#show-number").html("<h2> Time Remaining: " + number + " Seconds </h2>");
-
-
       //  Once number hits zero...
       if (number === 0) {
 
-        //  Alert the user that time is up.
-        $("#end").show();
+        //  Get score for question 1...
+        if (userAnswer1 == "correct") {
+          numCorrect++;
+          console.log(numCorrect);
+          $(".numCorrect").html("Correct Answers: " + numCorrect);
+
+         }
+
+        else if (userAnswer1 == "wrong") {
+          numIncorrect++;
+          console.log(numIncorrect);
+          $(".numIncorrect").html("Incorrect Answers: " + numIncorrect);
+
+         }
+
+        else {
+          numUnanswered++;
+          console.log(numUnanswered);
+          $(".numUnanswered").html("Unanswered Answers: " + numUnanswered);
+
+         }
+
+        //  Get score for question 2...
+        if (userAnswer2 == "correct") {
+          numCorrect++;
+          console.log(numCorrect);
+          $(".numCorrect").html("Correct Answers: " + numCorrect);
+
+         }
+
+        else if (userAnswer2 == "wrong") {
+          numIncorrect++;
+          console.log(numIncorrect);
+          $(".numIncorrect").html("Incorrect Answers: " + numIncorrect);
+
+         }
+
+        else {
+          numUnanswered++;
+          console.log(numUnanswered);
+          $(".numUnanswered").html("Unanswered Answers: " + numUnanswered);
+
+         }
+
+        //  Get score for question 3...
+        if (userAnswer3 == "correct") {
+          numCorrect++;
+          console.log(numCorrect);
+          $(".numCorrect").html("Correct Answers: " + numCorrect);
+
+         }
+
+        else if (userAnswer3 == "wrong") {
+          numIncorrect++;
+          console.log(numIncorrect);
+          $(".numIncorrect").html("Incorrect Answers: " + numIncorrect);
+
+         }
+
+        else {
+          numUnanswered++;
+          console.log(numUnanswered);
+          $(".numUnanswered").html("Unanswered Answers: " + numUnanswered);
+
+         }
+
+        //  Get score for question 4...
+        if (userAnswer4 == "correct") {
+          numCorrect++;
+          console.log(numCorrect);
+          $(".numCorrect").html("Correct Answers: " + numCorrect);
+
+         }
+
+        else if (userAnswer4 == "wrong") {
+          numIncorrect++;
+          console.log(numIncorrect);
+          $(".numIncorrect").html("Incorrect Answers: " + numIncorrect);
+
+         }
+
+        else {
+          numUnanswered++;
+          console.log(numUnanswered);
+          $(".numUnanswered").html("Unanswered Answers: " + numUnanswered);
+
+         }
+
+        //  Get score for question 5...
+        if (userAnswer5 == "correct") {
+          numCorrect++;
+          console.log(numCorrect);
+          $(".numCorrect").html("Correct Answers: " + numCorrect);
+
+         }
+
+        else if (userAnswer5 == "wrong") {
+          numIncorrect++;
+          console.log(numIncorrect);
+          $(".numIncorrect").html("Incorrect Answers: " + numIncorrect);
+
+         }
+
+        else {
+          numUnanswered++;
+          console.log(numUnanswered);
+          $(".numUnanswered").html("Unanswered Answers: " + numUnanswered);
+
+         }
+
+        //  Get score for question 6...
+        if (userAnswer1 == "correct") {
+          numCorrect++;
+          console.log(numCorrect);
+          $(".numCorrect").html("Correct Answers: " + numCorrect);
+
+         }
+
+        else if (userAnswer6 == "wrong") {
+          numIncorrect++;
+          console.log(numIncorrect);
+          $(".numIncorrect").html("Incorrect Answers: " + numIncorrect);
+
+         }
+
+        else {
+          numUnanswered++;
+          console.log(numUnanswered);
+          $(".numUnanswered").html("Unanswered Answers: " + numUnanswered);
+
+         }
+
+        // Send the scores to the "end" div to display on the page
+        $(".numCorrect").html("Correct Answers: " + numCorrect);
+        $(".numIncorrect").html("Incorrect Answers: " + numIncorrect);
+        $(".numUnanswered").html("Unanswered: " + numUnanswered);
+
+        //  Show the reults div and hide the questions div.
         $("#questions").hide();
+        $("#end").show();
       }
+
     }
 
-run();
-
-
-})
-    });
+  })
+});
